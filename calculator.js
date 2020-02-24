e26dae91151722b2578d2d2f53e93a9985bbca89
James Cardinale <jimmy@Jamess-MBP.hsd1.ma.comcast.net>
2020-02-24 12:30:56 -0500

    part-one

---
+++ calculator.js
@@ -0,0 +1,7 @@
+
+function calculate() {
+    var a = parseInt(document.getElementById("num1").value);
+    var b = parseInt(document.getElementById("num2").value);
+    var x = a + b;
+    document.getElementById("answer").innerHTML = x;
+}
